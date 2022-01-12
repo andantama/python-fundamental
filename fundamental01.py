b25ea8e868108dd4d0f7aa9ddb5c8ea5efbaeb0f
dcalneo <dancalneo@gmail.com>
2022-01-12 12:19:13 +0700

    Update comment "Perulangan"

--- fundamental01.py
+++ fundamental01.py
@@ -2,7 +2,7 @@
 Semua sintaksis dasar bahasa pemrograman terdiri dari:
 1. Sekuensial: langkah berurutan
 2. Percabangan: langkah melompat jika kondisi terpenuhi
-3. Pengulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 
 # Sekuensial
