6a0607660217fea13a448352f772f62f147002bc
dcalneo <dancalneo@gmail.com>
2022-01-12 09:50:26 +0700

    New fundamental-01

---
+++ fundamental01.py
@@ -0,0 +1,11 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Pengulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+print('Ibu memberi perintah,"Beli 1 botol susu"')
+print('Anak menjawab,"Ok"')
+print('Ibu memberi perintah tambahan, "Dan jika ada jual telur, maka beli juga 6 butir telur."')
+print('Anak merespon, "Ok,bu."')
+print("Anak pergi ke toko")
