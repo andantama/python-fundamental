fea68354e3d9fac9ae9ee5439b987e180db32084
dcalneo <dancalneo@gmail.com>
2022-01-17 13:39:28 +0700

    Update Loop dengan WHILE

--- Basic_Syntax_Loop_BacaBuku_WHILE.py
+++ Basic_Syntax_Loop_BacaBuku_WHILE.py
@@ -9,7 +9,7 @@ jumlah_buku_dibaca = 0
 print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
 
 while jumlah_buku_dibaca < jumlah_buku:
-    jumlah_buku_dibaca = jumlah_buku_dibaca + 1;
+    jumlah_buku_dibaca = jumlah_buku_dibaca + 1
     print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
 
 print(f"Jumlah buku yang sudah dibaca saat ini yaitu {jumlah_buku_dibaca}")
