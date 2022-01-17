fcac71838346132d8b50b0d96f25f332722abca7
dcalneo <dancalneo@gmail.com>
2022-01-17 11:12:19 +0700

    Update Loop

---
+++ Basic_Syntax_Loop_BacaBuku.py
@@ -0,0 +1,14 @@
+"""
+Program Perulangan Baca Buku
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua buku."')
+
+jumlah_buku_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
+
+for jumlah_buku_dibaca in range(1, jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_dibaca}")
