d398078f308e930f04bcb194291b82e013cd8c5b
dcalneo <dancalneo@gmail.com>
2022-01-28 16:11:36 +0700

    Update Belajar Loop WHILE Jumlah Tak Tentu

--- Basic_Syntax_Loop_BacaBuku_WHILE_Undefined_Count.py
+++ Basic_Syntax_Loop_BacaBuku_WHILE_Undefined_Count.py
@@ -4,3 +4,19 @@ Program Perulangan Baca Buku Sampai Paham dengan WHILE
 
 jumlah_buku = 10
 print('Ibu berkata, "Baca semua buku sampai paham.')
+
+total_jumlah_baca = 0
+
+jumlah_buku_dibaca_dipahami = 0
+print(f"jumlah buku yang telah dibaca dan dipahami {jumlah_buku_dibaca_dipahami}.")
+
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_dibaca_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_dibaca_dipahami + 1} belum paham.")
+    else:
+        jumlah_buku_dibaca_dipahami = jumlah_buku_dibaca_dipahami + 1
+        print(f"Buku ke {jumlah_buku_dibaca_dipahami} telah dibaca dan dipahami.")
+
+print(f"Total buku yang telah dibaca dan dipahami {jumlah_buku_dibaca_dipahami}.")
+
